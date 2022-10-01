71562f297de710699f23afdc4804bcb6d5b33d80
Krystofer Lando <krystofer02@gmail.com>
2022-10-01 13:48:56 +0200

    update readme, update runapp

--- runapp.sh
+++ runapp.sh
@@ -3,7 +3,7 @@
 VIRTUALENVS_DIR="C:\\users\\krystofair\\.virtualenvs\\"
 NAME=$(ls $VIRTUALENVS_DIR | grep "$(basename $PWD)-")
 PYTHON_HOME=$VIRTUALENVS_DIR\\$NAME
-export FLASK_APP=goFlask.py
-echo $FLASK_APP
-echo $PYTHON_HOME
+export FLASK_APP=.\\liczer4pg\\goFlask.py
+# echo $FLASK_APP
+# echo $PYTHON_HOME
 $PYTHON_HOME\\Scripts\\flask run
